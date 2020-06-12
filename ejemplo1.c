332c373acfe52edfa6a2738bbd5a88ee399e87bf
Francisco Bautista <jbautista@externos.santander.com.mx>
2020-06-11 20:27:38 -0500

    Nuevo ejemplo de tabla para conversion de temperaturas

---
+++ ejemplo1.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+// Definicion de funcion principal
+int main(void) {
+    // Declaracion de variables
+    float fahrenheit = 32.00;
+    float celcius;
+
+    celcius = (fahrenheit - 32) * (5/9);
+    printf("%f grados Fahrenheit = %f grados Celcius\n",fahrenheit,celcius);
+    fahrenheit = fahrenheit+1.00;
+    celcius = (fahrenheit - 32) * (5/9);
+    printf("%f grados Fahrenheit = %f grados Celcius\n",fahrenheit,celcius);
+    fahrenheit = fahrenheit+1.00;
+    celcius = (fahrenheit - 32) * (5/9);
+    printf("%f grados Fahrenheit = %f grados Celcius\n",fahrenheit,celcius);
+    fahrenheit = fahrenheit+1.00;
+    celcius = (fahrenheit - 32) * (5/9);
+    printf("%f grados Fahrenheit = %f grados Celcius\n",fahrenheit,celcius);
+    fahrenheit = fahrenheit+1.00;
+    celcius = (fahrenheit - 32) * (5/9);
+    printf("%f grados Fahrenheit = %f grados Celcius\n",fahrenheit,celcius);
+    fahrenheit = fahrenheit+1.00;
+    celcius = (fahrenheit - 32) * (5/9);
+    printf("%f grados Fahrenheit = %f grados Celcius\n",fahrenheit,celcius);
+    fahrenheit = fahrenheit+1.00;
+    celcius = (fahrenheit - 32) * (5/9);
+    printf("%f grados Fahrenheit = %f grados Celcius\n",fahrenheit,celcius);
+    fahrenheit = fahrenheit+1.00;
+    celcius = (fahrenheit - 32) * (5/9);
+    printf("%f grados Fahrenheit = %f grados Celcius\n",fahrenheit,celcius);
+    fahrenheit = fahrenheit+1.00;
+    celcius = (fahrenheit - 32) * (5/9);
+    printf("%f grados Fahrenheit = %f grados Celcius\n",fahrenheit,celcius);
+    fahrenheit = fahrenheit+1.00;
+
+    return 0;
+}
